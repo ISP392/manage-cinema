c4e84f879cebd1b61eaaf6759db6271024a7ba7c
baothiquoc <bquoc3002@gmail.com>
2024-05-19 23:44:06 +0700

    add connect database mysql

---
+++ src/main/java/DAO/DBcontext.java
@@ -0,0 +1,40 @@
+package DAO;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBcontext {
+    public static Connection getConnection() {
+        Connection c = null;
+        try{
+            //load .env file
+            Dotenv dotenv = Dotenv.configure()
+                    .directory("src/main/resources")
+                    .load();
+            // register the driver
+
+            //information connection
+            String url = dotenv.get("DB_URL");
+            String username = dotenv.get("DB_USERNAME");
+            String password = dotenv.get("DB_PASSWORD");
+
+            //create a connection
+            c = DriverManager.getConnection(url, username, password);
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return c;
+    }
+
+    public static void main(String[] args){
+        Connection c = DBcontext.getConnection();
+        if(c != null){
+            System.out.println("Connect successfully");
+        }else{
+            System.out.println("Connect failure");
+        }
+    }
+}
