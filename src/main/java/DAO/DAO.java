aa4f002582f17668d133583d4cb7e0ebd090bf6c
baothiquoc <bquoc3002@gmail.com>
2024-05-24 16:32:31 +0700

    udpate file dao

--- src/main/java/DAO/DAO.java
+++ src/main/java/DAO/DAO.java
@@ -4,10 +4,12 @@
  */
 package DAO;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import modal.Users;
 
 /**
@@ -15,36 +17,52 @@ import modal.Users;
  * @author MISS NGA
  */
 public class DAO extends DBcontext {
-    private Connection con;
-    private PreparedStatement prst;
-    private ResultSet rs;
 
-    public boolean checkUser(String username) {
-        String sql = "SELECT * FROM Users WHERE username = ?";
-        try {
-            PreparedStatement ps = con.prepareStatement(sql);
-            ps.setString(1, username);
-            ResultSet rs = ps.executeQuery();
-            if (rs.next()) {
-                return true;
-            }
-        } catch (SQLException e) {
-            System.out.println(e);
-        }
-        return false;
-    }
+
 
     public void add(Users u) {
-        String sql = "INSERT INTO Users (displayName, username, password, roleID) VALUES (?, ?, ?, 2)";
-        try (PreparedStatement ps = con.prepareStatement(sql)) {
+        String sql = "INSERT INTO Users (displayName, username, password, roleID, email, point) VALUES (?, ?, ?, 1,?,0)";
+        try{
+            PreparedStatement ps = getConnection().prepareStatement(sql);
             ps.setString(1, u.getDisplayName());
-            ps.setString(2, u.getUsername());
+            ps.setString(2, u.getUserName());
             ps.setString(3, u.getPassword());
+            ps.setString(4, u.getEmail());
             ps.executeUpdate();
-            rs.close();
-        } catch (SQLException e) {
-            System.out.println(e);
+        }catch(SQLException e) {
+            e.printStackTrace();
         }
+    }
+    public List<Users> getUsers(){
+        String sql = "SELECT * FROM Users";
+        try{
+            PreparedStatement ps = getConnection().prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            List<Users> list = new ArrayList<>();
+            while(rs.next()){
+                Users u = new Users();
+                u.setUserID(rs.getInt("userID"));
+                u.setDisplayName(rs.getString("displayName"));
+                u.setUserName(rs.getString("username"));
+                u.setPassword(rs.getString("password"));
+                u.setEmail(rs.getString("email"));
+                u.setPoint(rs.getInt("point"));
+                list.add(u);
+            }
+            return list;
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        DAO dao = new DAO();
+        dao.getUsers();
+        for(Users u : dao.getUsers()){
+            System.out.println(u.getUserName());
+        }
+        System.out.println(getConnection());
     }
 
     
