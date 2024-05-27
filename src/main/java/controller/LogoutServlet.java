710791ead403e8d644a2dd6414fa88b9417dc208
nga036 <qtran6859@gamil.com>
2024-05-27 09:39:12 +0700

    Committing changes before switching branches

---
+++ src/main/java/controller/LogoutServlet.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+/**
+ *
+ * @author MISS NGA
+ */
+@WebServlet(name="LogoutServlet", urlPatterns={"/logout"})
+public class LogoutServlet extends HttpServlet {
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        // Xóa session
+        session.invalidate();
+        // Chuyển hướng người dùng đến trang đăng nhập hoặc trang chính
+        request.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(request, response);
+    }
+}
+
+
