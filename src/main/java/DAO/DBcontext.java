5caa90fcfbcbc3281025c95d6411474a4f0f73c1
nga036 <qtran6859@gamil.com>
2024-05-27 14:52:46 +0700

    .

--- src/main/java/DAO/DBcontext.java
+++ src/main/java/DAO/DBcontext.java
@@ -6,7 +6,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBContext {
+public class DBContext{
     public Connection connection;
 
     public DBContext() {
