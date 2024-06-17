2db4b1af3f489fbd01b1cd3ee9287fb46743cdfc
baothiquoc <bquoc3002@gmail.com>
2024-06-18 03:46:01 +0700

    update forgot password

--- src/main/java/controller/NewPasswordServlet.java
+++ src/main/java/controller/NewPasswordServlet.java
@@ -2,9 +2,9 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
+import DAO.DAO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.NoSuchAlgorithmException;
@@ -21,36 +21,38 @@ import util.Encrypt;
  *
  * @author caoha
  */
-@WebServlet(name="NewPasswordServlet", urlPatterns={"/new_password"})
+@WebServlet(name = "NewPasswordServlet", urlPatterns = {"/new_password"})
 public class NewPasswordServlet extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet NewPasswordServlet</title>");  
+            out.println("<title>Servlet NewPasswordServlet</title>");
             out.println("</head>");
             out.println("<body>");
-            out.println("<h1>Servlet NewPasswordServlet at " + request.getContextPath () + "</h1>");
+            out.println("<h1>Servlet NewPasswordServlet at " + request.getContextPath() + "</h1>");
             out.println("</body>");
             out.println("</html>");
         }
-    } 
+    }
 
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -58,12 +60,13 @@ public class NewPasswordServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         request.getRequestDispatcher("/WEB-INF/views/newPass.jsp").forward(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -71,22 +74,24 @@ public class NewPasswordServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         Users u = (Users) request.getSession().getAttribute("user");
         String pass = request.getParameter("password");
+        DAO dao = new DAO();
         //encrypt pass
         try {
             pass = Encrypt.toSHA1(pass);
         } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        Users user = new Users(u.getEmail(), pass, 0);
-        request.getSession().setAttribute("user", user);
-        response.sendRedirect("verify");
+        dao.updatePasswordByEmail(u.getEmail(), pass);
+        request.getSession().removeAttribute("user");
+        request.getRequestDispatcher("/WEB-INF/views/successPass.jsp").forward(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
